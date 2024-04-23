851894b10efd02395349b60a2729f33953eb0b53
shinzo <pikachupokemon004@gmail.com>
2024-04-23 12:48:21 +0530

    4th commit

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ $(document).ready(function() {
         var scrollTop = $(this).scrollTop();
         var header = $('header');
 
-        // Define the background color when scrolled away from the top
+        
         var scrolledBackgroundColor = 'black'; 
 
-        // Define the background color when at the top
+       
         var topBackgroundColor = 'transparent';
 
         // Check if the user has scrolled away from the top of the page
@@ -54,30 +54,30 @@ $(document).ready(function() {
   });
 
 
-  $(document).ready(function() {
-    var headerLogo = $('.logo');
+//   $(document).ready(function() {
+//     var headerLogo = $('.logo');
 
-    // Function to adjust logo size for larger screens
-    function adjustLogoSize() {
-        headerLogo.addClass('adjusted'); // Add the 'adjusted' class to the logo for larger screens
-        $(window).off('scroll', adjustLogoSize); // Remove the scroll event listener
-    }
+//     // Function to adjust logo size for larger screens
+//     function adjustLogoSize() {
+//         headerLogo.addClass('adjusted'); // Add the 'adjusted' class to the logo for larger screens
+//         $(window).off('scroll', adjustLogoSize); // Remove the scroll event listener
+//     }
 
-    // Function to adjust logo size for smaller screens
-    function adjustLogoSizeMobile() {
-        headerLogo.addClass('adjusted-mobile'); // Add the 'adjusted-mobile' class to the logo for smaller screens
-        $(window).off('scroll', adjustLogoSizeMobile); // Remove the scroll event listener
-    }
+//     // Function to adjust logo size for smaller screens
+//     function adjustLogoSizeMobile() {
+//         headerLogo.addClass('adjusted-mobile'); // Add the 'adjusted-mobile' class to the logo for smaller screens
+//         $(window).off('scroll', adjustLogoSizeMobile); // Remove the scroll event listener
+//     }
 
-    // Listen for the scroll event
-    $(window).scroll(function() {
-        if ($(window).width() <= 768) {
-            adjustLogoSizeMobile(); // Apply logo size adjustment for smaller screens
-        } else {
-            adjustLogoSize(); // Apply logo size adjustment for larger screens
-        }
-    });
-});
+//     // Listen for the scroll event
+//     $(window).scroll(function() {
+//         if ($(window).width() <= 768) {
+//             adjustLogoSizeMobile(); // Apply logo size adjustment for smaller screens
+//         } else {
+//             adjustLogoSize(); // Apply logo size adjustment for larger screens
+//         }
+//     });
+// });
 
   
 
@@ -89,4 +89,10 @@ $(document).ready(function() {
 $(document).ready(function() {
     var currentYear = new Date().getFullYear();
     $('#currentYear').text(currentYear);
+});
+
+// navbar active
+
+$('#menu-btn').click(function() {
+    $('.navbar').toggleClass('active');
 });
